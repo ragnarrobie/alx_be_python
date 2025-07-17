7803455b787be7e719321b3b72d9314c8e125e39
Robson Taye <ragnarrobie@gmail.com>
2025-07-17 16:42:28 +0300

    try this

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,25 @@
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+opps = input("choose the operation (+,-,*,/):")
+sum = num1 + num2
+product= num1*num2
+difference = num1-num2
+
+match opps:
+    case "+":
+        print("The result is", sum)
+    case "-":
+        print("The difference is", difference)
+    case "*":
+        print("The product is", product)
+    case "/":
+        if num2==0:
+         print("Cannot divide by zero")
+        else:
+            quotient = num1/num2
+            print("The quotient is", quotient)
+    case _:
+        print("invalid operation")
+        
+    
+        
