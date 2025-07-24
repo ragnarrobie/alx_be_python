d6367dffd1b36f32414a76ff9293d904f44f2cb3
Robson Taye <ragnarrobie@gmail.com>
2025-07-24 10:52:59 +0300

    1

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1,num2,operation):
+    if operation=="add":
+        return num1 + num2
+    elif operation=="subtract":
+        return num1 - num2 
+    elif operation=="multiply":
+        return num1*num2
+    elif operation=="divide":
+        if num1 % 2==0:
+         return num1/num2
+    else:
+        return "undefined"
+    
