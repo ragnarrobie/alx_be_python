94444ebc3bea8b486b2a8ec9ee72fcf61099dd1d
Robson Taye <ragnarrobie@gmail.com>
2025-07-24 10:59:16 +0300

    5

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,8 +6,10 @@ def perform_operation(num1, num2, operation):
     elif operation=="multiply":
         return num1*num2
     elif operation=="divide":
-        if num2 != 0:
+        if num2 !=0:
          return num1/num2
+        else:
+            return "Error: number cant be divided by 0"
     else:
         return "undefined"
     
