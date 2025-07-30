92b8224153e68a96791719a3c4af92d9ef40f2ff
Robson Taye <ragnarrobie@gmail.com>
2025-07-30 17:16:06 +0300

    1

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,15 +1,19 @@
 class BankAccount:
     def __init__(self,initial_balance):
         self.actual_balance = initial_balance
+    def deposit(self,amount):
+        if amount>0:
+            self.account_balance += amount
+            return True
+        return False
+    def withdraw(self,amount):
+        
+        if 0< amount<=self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
+
     def display_balance(self):
         print(f"{self.actual_balance}")
 
-    def deposit(self,amount):
-        account_balance += amount
-    def withdraw(self,amount):
-        boo = self.account_balance - amount
-        if boo<0:
-            return False, amount
-        else:
-            return True,boo
     
